0bfdb1d2dcee2c2231b184ca1257996bd29f0ca2
MaRtIn-9202 <73021358+MaRtIn-9202@users.noreply.github.com>
2021-02-28 19:11:24 +0100

    Update backup.sh

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -3,13 +3,11 @@
 
 DATE=$(date +%Y%m%d-%H%M%S)
 
-DBPATH=$HOME/pandaz/bdd.db
-DESTDIR=$HOME/pandaz/backups
+DBPATH=$HOME/path/too/bdd.db
+DESTDIR=$HOME/path/too/backups/
 
 
 db=$(basename $DBPATH)
 
 
 sqlite3 $DBPATH .dump | gzip -c > ${DESTDIR}/${db%%.db}-${DATE}.sql.gz
-
-
